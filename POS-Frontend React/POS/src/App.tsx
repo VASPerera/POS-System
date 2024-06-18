ff5bf5be2fd7a15f504bf0628aad9aaaeb1b9a0a
VASPerera <avishka1999perera@gmail.com>
2024-06-18 15:32:17 +0530

    add navbar and logo

--- POS-Frontend React/POS/src/App.tsx
+++ POS-Frontend React/POS/src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Customer from "./components/Customer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <BrowserRouter>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+              <div className="container-fluid">
+                <div className="navbar-brand" >
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/1/13/Wattpad-logo-vector.svg" alt="" className="logo" />
+                </div>
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarNavAltMarkup"
+                  aria-controls="navbarNavAltMarkup"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div className="navbar-nav">
+                    <a className="nav-link active" aria-current="page" href="#">
+                      Home
+                    </a>
+                    <a className="nav-link" href="#">
+                      Features
+                    </a>
+                    <a className="nav-link" href="#">
+                      Pricing
+                    </a>
+                    <a className="nav-link disabled" aria-disabled="true">
+                      Disabled
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </nav>
+          </div>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/customer" element={<Customer></Customer>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
