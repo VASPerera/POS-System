49872341fcd158f769abb92e2c9f64bde309a0dc
VASPerera <avishka1999perera@gmail.com>
2024-06-20 16:59:50 +0530

    6/19

--- POS-Frontend React/POS/src/components/Home.tsx
+++ POS-Frontend React/POS/src/components/Home.tsx
@@ -1,9 +1,57 @@
-
+import DefaultCard from "./cards/DefaultCard";
+import DefaultChart from "./cards/DefaultChart";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+          <DefaultCard
+            tumbnail='https://img.freepik.com/free-photo/businesspeople-having-discussion-office_107420-84849.jpg?t=st=1718880076~exp=1718883676~hmac=ce18e8bfeb30cfbb831a5b710f18e8c1c024e184a7e6163a46afd3d3202fd369&w=996'
+            title = 'Customers'
+            description=''
+            value='250'
+            key={1}
+          />
+        </div>
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+          <DefaultCard
+            tumbnail='https://img.freepik.com/free-photo/flat-lay-photo-spa-still-life-skin-care-items_169016-5050.jpg?t=st=1718880164~exp=1718883764~hmac=87ae77f3c09ad0d6eaafd083ef7ec03f1cb69d44365641452c3ffe65de4517ed&w=1060'
+            title = 'Products'
+            description=''
+            value='350'
+            key={2}
+          />
+        </div>
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+          <DefaultCard
+            tumbnail='https://img.freepik.com/premium-photo/woman-standing-front-laptop-computer-suitable-technology-businessrelated-projects_153912-184253.jpg?w=996'
+            title = 'Oders'
+            description='Sdd'
+            value='450'
+            key={3}
+          />
+        </div>
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+          <DefaultCard
+            tumbnail='https://img.freepik.com/premium-photo/growth-money-arrow-success-financial-business-coin-concept-investment-earnings-background-with-profit-graph-finance-development-increase-economic-market-chart-digital-stock-currency-strategy_79161-2472.jpg?w=1380'
+            title = 'Income'
+            description='Sdd'
+            value='550'
+            key={4}
+          />
+        </div>
+        
+      </div>
+      <br></br>
+      <div className="row">
+        <div className="col-12 col-md-6">
+            <div className="context">
+                <DefaultChart></DefaultChart>
+            </div>
+        </div>
+        <div className="col-12 col-md-6"></div>
+      </div>
     </div>
-  )
+  );
 }
---
+++ POS-Frontend React/POS/src/components/cards/DefaultCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+interface DefaultCardData {
+    tumbnail : String,
+    title : String,
+    description : String,
+    value : String
+}
+
+export default function DefaultCard(props: DefaultCardData) {
+  const maxWidthStyle: React.CSSProperties = {
+    maxWidth: "540px",
+  };
+  return (
+    <div>
+      <div className="card mb-3" style={maxWidthStyle}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src={props.tumbnail} className="img-fluid rounded-start" alt={props.title} />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{props.title}</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">
+                <small className="text-body-secondary">
+                  {props.value}
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
